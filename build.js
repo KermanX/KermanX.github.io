cca4a3ec766c223b598f24204b430f2a2e488397
_Kerman <KermanX@qq.com>
2024-02-25 22:40:54 +0800

    fix: char code

--- build.js
+++ build.js
@@ -1,23 +1,29 @@
-import fg from 'fast-glob';
-import fs from 'fs';
+import fg from "fast-glob";
+import fs from "fs";
 
-const template = fs.readFileSync('template.html', 'utf-8');
+const template = fs.readFileSync("template.html", "utf-8");
 
-const files = fg.sync('src/**/*.md');
+const files = fg.sync("src/**/*.md");
 
 const toc = `
 ## 目录
 
-${files.map(file => '- https://kermanx.github.io' + file.replace(/^src/, '').replace(/.md$/, '.html')).join('\n')}
-`
+${files
+  .map(
+    (file) =>
+      "- https://kermanx.github.io" +
+      file.replace(/^src/, "").replace(/.md$/, ".html")
+  )
+  .join("\n")}
+`;
 
-for(const file of files) {
-  console.log('building', file);
-  const content = fs.readFileSync(file, 'utf-8') + toc
-  const html = template.replace('CONTENT', JSON.stringify(content));
-  const out = file.replace(/^src/, 'docs').replace(/.md$/, '.html');
-  fs.mkdirSync(out.replace(/\/[^/]+$/, ''), { recursive: true });
+for (const file of files) {
+  console.log("building", file);
+  const content = fs.readFileSync(file, "utf-8") + toc;
+  const html = template.replace("CONTENT", JSON.stringify([...content]));
+  const out = file.replace(/^src/, "docs").replace(/.md$/, ".html");
+  fs.mkdirSync(out.replace(/\/[^/]+$/, ""), { recursive: true });
   fs.writeFileSync(out, html);
 }
 
-console.log('done');
+console.log("done");
